2c0db2ff4086683ad5d1fa45a22972c376efffe0
havennb <61809840+havennb@users.noreply.github.com>
2020-11-19 13:44:54 -0600

    Add files via upload

---
+++ 404-page.php
@@ -0,0 +1,17 @@
+<?php
+
+// 
+
+?>
+<!DOCTYPE html>
+<htML>
+    <head>
+        <title>404 missing link</title>
+        <link rel = 'stylesheet' type = 'text/css' href = 'css/style.css'>
+    </head>
+    <body>
+        <div class = 'main'>
+            <div class = 'title-box'><h1 id = 'main-title'><center>404 missing link</center></h1></div>
+        </div>
+    </body>
+</htML>
