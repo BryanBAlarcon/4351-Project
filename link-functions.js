eb720fc9ead8b29fb204196803bb1615d3e437cc
havennb <61809840+havennb@users.noreply.github.com>
2020-11-30 13:25:59 -0600

    Add files via upload

---
+++ link-functions.js
@@ -0,0 +1,24 @@
+function listFunctions(text) {
+    //this function will fill the the link-select options depening on what admin role is in the url
+    var adminRole = '';
+    options = '';
+    if(adminRole == 'ADMIN') {
+        options == '<option>Manage User Accounts</option><option>Assign Roles</option><option>Help Desk</option>'
+    }
+    if(adminRole == 'FINANCE_ADMIN') {
+        options == '<option>Finance Reports</option><option>Accounts Payable</option><option>Accounts Receivable</option><option>Tax</option>'
+    }
+    if(adminRole == 'SALES_ADMIN') {
+        options == '<option>Sales Reports</option><option>Sales Leads</option><option>Sales Demo</option>'
+    }
+    if(adminRole == 'HR_ADMIN') {
+        options == '<option>New Hire On-Boarding</option><option>Benefits</option><option>Payroll</option><option>Off-boarding</option><option>HR Reports</option>'
+    }
+    if(adminRole == 'TECH_ADMIN') {
+        options == '<option>Application Monitoring</option><option>Tech Support</option><option>App Development</option><option>Add Admin</option><option>Release Management</option>'
+    }
+}
+
+function openLinkPage(text) {
+    //this function will open a 404 page for the link after clicking go
+}
---
+++ login-functions.js
@@ -0,0 +1,4 @@
+function getCorrectLinks(text) {
+    //this function needs to get the value from the admin-select option and pass it into the url
+
+}
